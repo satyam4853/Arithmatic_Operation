b591e45f2b8eb57ddb6a01731b28d1d511544e34
Your Name <you@example.com>
2021-07-26 13:31:57 +0530

    sorted array in ascending order

--- Arithamtic_Operation.sh
+++ Arithamtic_Operation.sh
@@ -43,3 +43,17 @@ do
 done
 echo "Array in Decending order:";
 echo ${resultArray[@]};
+for ((i=0;i<3;i++))
+do
+        for ((j=i+1;j<4;j++))
+        do
+                if ((${resultArray[i]} > ${resultArray[j]}))
+                then
+                        temp=${resultArray[$j]};
+                        resultArray[$j]=${resultArray[$i]};
+                        resultArray[$i]=$temp;
+                fi
+        done
+done
+echo "Array in Decending order:";
+echo ${resultArray[@]};
